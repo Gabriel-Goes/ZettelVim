15c5a897b3572f9c9622fa9e7864a302f309d043
ggrl <gabrielgoes@usp.br>
2024-03-25 08:09:42 -0300

    commit ot ipt

--- lua/zettelvim/config.lua
+++ lua/zettelvim/config.lua
@@ -36,8 +36,8 @@ function M.VisualCall()
 end
 
 function M.setup(opts)
-    vim.api.nvim_set_keymap('v', opts.visual_mode_keymap or 'bf', '<cmd>lua require("zettelvim.config").VisualCall()<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', opts.normal_mode_keymap or '<leader>bf', '<cmd>lua require("zettelvim.config").NormalCall()<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('v', opts.visual_mode_keymap or 'qf', '<cmd>lua require("zettelvim.config").VisualCall()<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', opts.normal_mode_keymap or '<leader>qf', '<cmd>lua require("zettelvim.config").NormalCall()<CR>', { noremap = true, silent = true })
 end
 
 return M
